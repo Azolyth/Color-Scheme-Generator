48bb605283a9504d8362f564c5ec11f79cc89c4b
Silas <azolyth@gmail.com>
2022-08-24 20:28:41 -0700

    Styled and adjusted div sizing

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 'use strict';
 
-console.log('index.js is running...');
-
 const getColorBtn = document.getElementById('get-color-btn');
 const colorPicker = document.getElementById('color-picker');
 const schemeSelect = document.getElementById('scheme-select');
 const colorScheme = document.getElementById('color-scheme');
+const displayHex = document.getElementById('display-hex');
 
 const getColorScheme = async () => {
   const hexValue = colorPicker.value.slice(1);
   const schemeValue = schemeSelect.value;
 
-  console.log(hexValue);
   const response = await fetch(
     `https://www.thecolorapi.com/scheme?hex=${hexValue}&mode=${schemeValue}&count=6`
   );
   const data = await response.json();
-  console.log(schemeSelect.value);
-  console.log(data);
-
-  colorScheme.innerHTML = `
-  
-    <img class="color-scheme-img" src=${data.image.bare} />
-  `;
+  let colorsHtml = '';
+  for (let i = 0; i < data.colors.length; i++) {
+    colorsHtml += `
+      <div>
+        <img class="colors" src="${data.colors[i].image.bare}" />
+        <p class="hex" >${data.colors[i].hex.value}</p>
+      </div>
+    `;
+  }
+  colorScheme.innerHTML = colorsHtml;
 };
 
+getColorScheme();
 getColorBtn.addEventListener('click', getColorScheme);
